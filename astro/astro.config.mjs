6a19399ad4c022a717646e5032ea8c8832bd752c
Neil Master <yeehawlerz101@gmail.com>
2026-06-20 20:21:35 -0400

    fix urls

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://yeehawlerz101.github.io',
+    base: '/ao-profile',
     vite: {
         plugins: [tailwindcss()]
     },
